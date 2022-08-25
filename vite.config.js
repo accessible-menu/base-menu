8cd82a1341def097008aebc9ecd936f074c3daeb
Nick Milton <nickdjm@gmail.com>
2022-08-25 16:11:17 -0400

    build(vite): add build setup

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/BaseMenu.js"),
+      name: "BaseMenu",
+      formats: ["iife", "es"],
+      fileName: format => `base-menu.${format}.js`,
+    },
+    minify: "terser",
+  },
+});
